c09b7220a9d625d5d8b02747b9cbd01799330bc2
chxing <chxing@seas.upenn.edu>
2026-05-13 18:53:22 -0400

    WIP: add go2 flat terrain environment for training on gpu

--- mujoco_playground/_src/locomotion/__init__.py
+++ mujoco_playground/_src/locomotion/__init__.py
@@ -32,6 +32,8 @@ from mujoco_playground._src.locomotion.go1 import getup as go1_getup
 from mujoco_playground._src.locomotion.go1 import handstand as go1_handstand
 from mujoco_playground._src.locomotion.go1 import joystick as go1_joystick
 from mujoco_playground._src.locomotion.go1 import randomize as go1_randomize
+from mujoco_playground._src.locomotion.go2 import joystick as go2_joystick
+from mujoco_playground._src.locomotion.go2 import randomize as go2_randomize
 from mujoco_playground._src.locomotion.h1 import inplace_gait_tracking as h1_inplace_gait_tracking
 from mujoco_playground._src.locomotion.h1 import joystick_gait_tracking as h1_joystick_gait_tracking
 from mujoco_playground._src.locomotion.op3 import joystick as op3_joystick
@@ -68,6 +70,9 @@ _envs = {
     "Go1Getup": go1_getup.Getup,
     "Go1Handstand": go1_handstand.Handstand,
     "Go1Footstand": go1_handstand.Footstand,
+    "Go2JoystickFlatTerrain": functools.partial(
+        go2_joystick.Joystick, task="flat_terrain"
+    ),
     "H1InplaceGaitTracking": h1_inplace_gait_tracking.InplaceGaitTracking,
     "H1JoystickGaitTracking": h1_joystick_gait_tracking.JoystickGaitTracking,
     "Op3Joystick": op3_joystick.Joystick,
@@ -102,6 +107,7 @@ _cfgs = {
     "Go1Getup": go1_getup.default_config,
     "Go1Handstand": go1_handstand.default_config,
     "Go1Footstand": go1_handstand.default_config,
+    "Go2JoystickFlatTerrain": go2_joystick.default_config,
     "H1InplaceGaitTracking": h1_inplace_gait_tracking.default_config,
     "H1JoystickGaitTracking": h1_joystick_gait_tracking.default_config,
     "Op3Joystick": op3_joystick.default_config,
@@ -126,6 +132,7 @@ _randomizer = {
     "Go1Getup": go1_randomize.domain_randomize,
     "Go1Handstand": go1_randomize.domain_randomize,
     "Go1Footstand": go1_randomize.domain_randomize,
+    "Go2JoystickFlatTerrain": go2_randomize.domain_randomize,
     "T1JoystickFlatTerrain": t1_randomize.domain_randomize,
     "T1JoystickRoughTerrain": t1_randomize.domain_randomize,
 }
--- mujoco_playground/config/locomotion_params.py
+++ mujoco_playground/config/locomotion_params.py
@@ -50,7 +50,11 @@ def brax_ppo_config(
       num_resets_per_eval=10,
   )
 
-  if env_name in ("Go1JoystickFlatTerrain", "Go1JoystickRoughTerrain"):
+  if env_name in (
+      "Go1JoystickFlatTerrain",
+      "Go1JoystickRoughTerrain",
+      "Go2JoystickFlatTerrain",
+  ):
     rl_config.num_timesteps = 200_000_000
     rl_config.num_evals = 10
     rl_config.network_factory = config_dict.create(
